0b9b5b4839a510a752f8a971dad7deab8081d8ac
MLYNDHG <2674268221@qq.com>
2022-08-26 14:23:13 +0800

    springboot vue
    Signed-off-by: MLYNDHG <2674268221@qq.com>

---
+++ Host Resources/springBootHr/src/test/java/com/springboothr/vhr/SpringBootHrApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springboothr.vhr;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootHrApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
